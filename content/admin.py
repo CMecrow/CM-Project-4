19704a2d807a31df4aa5774e85efbea38f20d1b3
Chris Mecrow <mecrow@live.co.uk>
2022-05-07 12:43:14 +0000

    Added filters and searches to the admin page along with admin comment moderation

--- content/admin.py
+++ content/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on', 'author')
+    search_fields = ['content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'body')
+    search_fields = ('author', 'body')
 
--- content/models.py
+++ content/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Archived"), (1, "Published"))
 
 class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     created_on = models.DateTimeField(auto_now_add=True)
