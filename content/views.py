97cfee555552a63af862a8ab565cc0b3e7ff2dc5
Chris Mecrow <mecrow@live.co.uk>
2022-05-13 12:52:35 +0000

    Create post_detail template and edit views to display individual post

--- content/views.py
+++ content/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Post
+from .models import Post, Comment
 
 class PostList(generic.ListView):
     model = Post
@@ -13,14 +13,14 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.order_by('created_on')
+        comments = Comment.objects.order_by('created_on')
         voted = False
         if post.votes.filter(id=self.request.user.id).exists():
             voted = True
         
         return render(
             request,
-            "content_detail.html",
+            "post_detail.html",
             {
                 "post": post,
                 "comments": comments,
