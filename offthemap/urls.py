f2f5c2bb14a3cb63e159e40d72bc32e7b82c8108
Chris Mecrow <mecrow@live.co.uk>
2022-05-14 11:37:35 +0000

    Create second block of posts in index template, add authentication and sign up / in / out

--- offthemap/urls.py
+++ offthemap/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('content.urls'), name='content_urls')
+    path('', include('content.urls'), name='content_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
